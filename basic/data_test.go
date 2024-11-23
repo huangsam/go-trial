ee225616727312d193a0449c30c8452187b9a576
Samuel Huang <samhuang91@gmail.com>
2024-11-23 13:32:24 -0800

    Create map test

--- basic/data_test.go
+++ basic/data_test.go
@@ -12,3 +12,23 @@ func TestSliceValuesArePositive(t *testing.T) {
 		}
 	}
 }
+
+func TestMapCanAccessValues(t *testing.T) {
+	myMap := map[string]int{
+		"apple":  1,
+		"banana": 2,
+		"cherry": 3,
+	}
+	if myMap["apple"] != 1 {
+		t.Errorf("Expected apple value to be 1, got %d", myMap["apple"])
+	}
+	if myMap["banana"] != 2 {
+		t.Errorf("Expected banana value to be 2, got %d", myMap["banana"])
+	}
+	if myMap["cherry"] != 3 {
+		t.Errorf("Expected cherry value to be 3, got %d", myMap["cherry"])
+	}
+	if _, ok := myMap["bogus"]; ok {
+		t.Error("Expected bogus to be missing")
+	}
+}
