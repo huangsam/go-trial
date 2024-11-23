9bf2f021ad479424ea376b187c69fa0ebf41f7f9
Samuel Huang <samhuang91@gmail.com>
2024-11-23 12:30:03 -0800

    Add benchmark for IsOlderThan

--- basic/person.go
+++ basic/person.go
@@ -12,3 +12,7 @@ func GetAge(p Person) int32 {
 func SetAge(p *Person, newAge int32) {
 	p.age = newAge
 }
+
+func IsOlderThan(first Person, target int32) bool {
+	return first.age >= target
+}
--- basic/person_test.go
+++ basic/person_test.go
@@ -4,21 +4,24 @@ import (
 	"testing"
 )
 
-func TestGetNameDirectly(t *testing.T) {
-	var expected string = "Sally"
-	person := Person{expected, 0}
-	actual := person.Name
-	if actual != expected {
-		t.Errorf("Expected name %s but got %s", expected, actual)
-	}
-}
+func TestGetInfo(t *testing.T) {
+	var expectedName string = "John"
+	var expectedAge int32 = 12
+	person := Person{expectedName, expectedAge}
 
-func TestGetAge(t *testing.T) {
-	var expected int32 = 12
-	person := Person{"John", expected}
-	actual := GetAge(person)
-	if actual != expected {
-		t.Errorf("Expected age %d but got %d", expected, actual)
+	actualName := person.Name
+	if actualName != expectedName {
+		t.Errorf("Expected name %s but got %s", expectedName, actualName)
+	}
+
+	actualAge := GetAge(person)
+	if actualAge != expectedAge {
+		t.Errorf("Expected age %d but got %d", expectedAge, actualAge)
+	}
+
+	isOlderThanSeventeen := IsOlderThan(person, 17)
+	if isOlderThanSeventeen {
+		t.Errorf("Expected a young one, but got someone with age %d", GetAge(person))
 	}
 }
 
@@ -36,5 +39,12 @@ func BenchmarkGetAge(b *testing.B) {
 	person := Person{"Bob", 0}
 	for i := 0; i < b.N; i++ {
 		GetAge(person)
+	}
+}
+
+func BenchmarkIsOlderThan(b *testing.B) {
+	person := Person{"Jerry", 0}
+	for i := 0; i < b.N; i++ {
+		IsOlderThan(person, 18)
 	}
 }
