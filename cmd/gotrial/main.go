0274ac94ab891c59266b4a01644e81a89ee876ac
Samuel Huang <samhuang91@gmail.com>
2025-01-19 10:02:59 -0800

    Refactor gotrial logic

--- cmd/gotrial/main.go
+++ cmd/gotrial/main.go
@@ -6,11 +6,17 @@ import (
 	basic "io.huangsam/trial/pkg/basicintro"
 )
 
+// main is the entry point of the application.
 func main() {
 	log.Info().Msg(basic.GreetWorld())
 	log.Info().Msg(basic.GreetName("Peter"))
 
 	circle := abstraction.Circle{Radius: 6}
+	logCircleSize(circle)
+}
+
+// logCircleSize logs the size of the given circle.
+func logCircleSize(circle abstraction.Circle) {
 	size := abstraction.Classify(circle)
 	log.Info().Msgf("Circle size is %v", size)
 }
