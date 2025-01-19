2e855b1623172dfef1481a34209ad4b32831f021
Samuel Huang <samhuang91@gmail.com>
2025-01-18 22:57:07 -0800

    Add comments to test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -eu
 
+# Default mode is "default"
 mode="${1:-default}"
 
+# Run benchmarks for all packages
 if [[ "$mode" == "bench" ]]; then
     go test -bench=. ./...
+
+# Run tests for the "concurrency" package 10 times
 elif [[ "$mode" == "concurrency" ]]; then
     go test -count=10 io.huangsam/trial/pkg/concurrency
+
+# Run tests for all packages
 else
     go test ./...
 fi
