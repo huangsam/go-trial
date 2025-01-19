9dc10a019445ce0d80b5b86fd73b0751247c1baa
Samuel Huang <samhuang91@gmail.com>
2025-01-19 09:26:15 -0800

    Add circle logging in gotrial

--- cmd/gotrial/main.go
+++ cmd/gotrial/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/rs/zerolog/log"
+	"io.huangsam/trial/pkg/abstraction"
 	basic "io.huangsam/trial/pkg/basicintro"
 )
 
 func main() {
 	log.Info().Msg(basic.GreetWorld())
 	log.Info().Msg(basic.GreetName("Peter"))
+
+	circle := abstraction.Circle{Radius: 6}
+	size := abstraction.Classify(circle)
+	log.Info().Msgf("Circle size is %v", size)
 }
