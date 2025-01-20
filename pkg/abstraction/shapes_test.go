0d7a447252fb5097322c579c1e73cd5427f54e8b
Samuel Huang <samhuang91@gmail.com>
2025-01-19 17:42:39 -0800

    Remove redundant test comments

--- pkg/abstraction/shapes_test.go
+++ pkg/abstraction/shapes_test.go
@@ -8,25 +8,21 @@ import (
 	"io.huangsam/trial/pkg/abstraction"
 )
 
-// TestRectangleArea tests the Area method of the Rectangle struct
 func TestRectangleArea(t *testing.T) {
 	rect := abstraction.Rectangle{Width: 5, Height: 3}
 	assert.Equal(t, 15.0, rect.Area())
 }
 
-// TestRectanglePerimeter tests the Perimeter method of the Rectangle struct
 func TestRectanglePerimeter(t *testing.T) {
 	rect := abstraction.Rectangle{Width: 5, Height: 3}
 	assert.Equal(t, 16.0, rect.Perimeter())
 }
 
-// TestCircleArea tests the Area method of the Circle struct
 func TestCircleArea(t *testing.T) {
 	circle := abstraction.Circle{Radius: 2}
 	assert.Equal(t, math.Pi*4, circle.Area())
 }
 
-// TestCirclePerimeter tests the Perimeter method of the Circle struct
 func TestCirclePerimeter(t *testing.T) {
 	circle := abstraction.Circle{Radius: 2}
 	assert.Equal(t, 4*math.Pi, circle.Perimeter())
