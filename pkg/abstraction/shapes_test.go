d22194f4848f4cdaf073ea08bc653c8405a33cd0
Samuel Huang <samhuang91@gmail.com>
2025-01-19 17:25:28 -0800

    Shorten tests for shapes module

--- pkg/abstraction/shapes_test.go
+++ pkg/abstraction/shapes_test.go
@@ -11,31 +11,23 @@ import (
 // TestRectangleArea tests the Area method of the Rectangle struct
 func TestRectangleArea(t *testing.T) {
 	rect := abstraction.Rectangle{Width: 5, Height: 3}
-	expectedArea := 15.0
-	actualArea := rect.Area()
-	assert.Equal(t, expectedArea, actualArea)
+	assert.Equal(t, 15.0, rect.Area())
 }
 
 // TestRectanglePerimeter tests the Perimeter method of the Rectangle struct
 func TestRectanglePerimeter(t *testing.T) {
 	rect := abstraction.Rectangle{Width: 5, Height: 3}
-	expectedPerimeter := 16.0
-	actualPerimeter := rect.Perimeter()
-	assert.Equal(t, expectedPerimeter, actualPerimeter)
+	assert.Equal(t, 16.0, rect.Perimeter())
 }
 
 // TestCircleArea tests the Area method of the Circle struct
 func TestCircleArea(t *testing.T) {
 	circle := abstraction.Circle{Radius: 2}
-	expectedArea := math.Pi * 4
-	actualArea := circle.Area()
-	assert.Equal(t, expectedArea, actualArea)
+	assert.Equal(t, math.Pi*4, circle.Area())
 }
 
 // TestCirclePerimeter tests the Perimeter method of the Circle struct
 func TestCirclePerimeter(t *testing.T) {
 	circle := abstraction.Circle{Radius: 2}
-	expectedPerimeter := 4 * math.Pi
-	actualPerimeter := circle.Perimeter()
-	assert.Equal(t, expectedPerimeter, actualPerimeter)
+	assert.Equal(t, 4*math.Pi, circle.Perimeter())
 }
