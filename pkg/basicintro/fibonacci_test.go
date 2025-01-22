8973deeedccddfb5c4f7d51adf0ebb28270ee5a3
Samuel Huang <samhuang91@gmail.com>
2025-01-21 19:56:22 -0800

    Add name to fibonacci test cases

--- pkg/basicintro/fibonacci_test.go
+++ pkg/basicintro/fibonacci_test.go
@@ -9,22 +9,23 @@ import (
 
 func TestFibonacci(t *testing.T) {
 	testCases := []struct {
+		name     string
 		input    int
 		expected int
 	}{
-		{0, 1},
-		{1, 1},
-		{2, 2},
-		{3, 3},
-		{5, 8},
-		{10, 89},
+		{"Zero", 0, 1},
+		{"One", 1, 1},
+		{"Two", 2, 2},
+		{"Three", 3, 3},
+		{"Five", 5, 8},
+		{"Ten", 10, 89},
 	}
 	for _, tc := range testCases {
-		t.Run("Iterative", func(t *testing.T) {
+		t.Run("Iterative "+tc.name, func(t *testing.T) {
 			actual := basicintro.FibonacciIterative(tc.input)
 			assert.Equal(t, tc.expected, actual)
 		})
-		t.Run("Recursive", func(t *testing.T) {
+		t.Run("Recursive "+tc.name, func(t *testing.T) {
 			actual := basicintro.FibonacciRecursive(tc.input)
 			assert.Equal(t, tc.expected, actual)
 		})
