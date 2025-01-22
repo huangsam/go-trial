dba35fa33f20ab3580482ada869acfe65020e1c4
Samuel Huang <samhuang91@gmail.com>
2025-01-21 20:48:12 -0800

    Refactor logging in gotrial

--- cmd/gotrial/main.go
+++ cmd/gotrial/main.go
@@ -12,14 +12,15 @@ import (
 
 // main is the entry point of the application.
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	logger.Info(basicintro.GreetWorld())
-	logger.Info(basicintro.GreetName("Peter"))
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+
+	slog.Info(basicintro.GreetWorld())
+	slog.Info(basicintro.GreetName("Peter"))
 
 	circle := abstraction.Circle{Radius: 6}
 	size := abstraction.Classify(circle)
-	logger.Info(fmt.Sprintf("Circle size is %v", size))
+	slog.Info(fmt.Sprintf("Circle size is %v", size))
 
 	answers := concurrency.GetAnswersWithChannels()
-	logger.Info("Retrieved answers with channels", "answers", answers)
+	slog.Info("Retrieved answers with channels", "answers", answers)
 }
