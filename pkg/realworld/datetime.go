78bb72964c5bd3979f4a89691781cbac0fc73ea3
Samuel Huang <samhuang91@gmail.com>
2025-01-22 02:53:02 -0800

    Fix semantics for datetime

--- pkg/realworld/datetime.go
+++ pkg/realworld/datetime.go
@@ -4,11 +4,11 @@ import (
 	"time"
 )
 
-// CompareDatetime compares two datetime values.
-func CompareDatetime(datetime1 time.Time, datetime2 time.Time) int {
-	if datetime1.Before(datetime2) {
+// CompareTime compares two time values.
+func CompareTime(time1 time.Time, time2 time.Time) int {
+	if time1.Before(time2) {
 		return -1
-	} else if datetime1.After(datetime2) {
+	} else if time1.After(time2) {
 		return 1
 	} else {
 		return 0
--- pkg/realworld/datetime_test.go
+++ pkg/realworld/datetime_test.go
@@ -7,23 +7,21 @@ import (
 	"github.com/huangsam/go-trial/pkg/realworld"
 )
 
-func TestCompareDatetime(t *testing.T) {
+func TestCompareTime(t *testing.T) {
 	now := time.Now()
 	previous := now.Add(-time.Hour * 24)
-
 	testCases := []struct {
-		datetime1 time.Time
-		datetime2 time.Time
-		expected  int
-		message   string
+		time1    time.Time
+		time2    time.Time
+		expected int
+		message  string
 	}{
-		{previous, now, -1, "datetime1 should be before datetime2"},
-		{now, previous, 1, "datetime1 should be after datetime2"},
-		{now, now, 0, "datetime1 should be equal to datetime2"},
+		{previous, now, -1, "time1 should be before time2"},
+		{now, previous, 1, "time1 should be after time2"},
+		{now, now, 0, "time1 should be equal to time2"},
 	}
-
 	for _, tc := range testCases {
-		if result := realworld.CompareDatetime(tc.datetime1, tc.datetime2); result != tc.expected {
+		if result := realworld.CompareTime(tc.time1, tc.time2); result != tc.expected {
 			t.Error(tc.message)
 		}
 	}
