93cbd21ea3080f0f1c3c41ab24e3238b39551d12
Samuel Huang <samhuang91@gmail.com>
2025-01-26 08:32:40 -0800

    Revise setup script and use in CI

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eu
 
-brew install golangci-lint gotestsum mockery staticcheck
+mode="${1:-default}"
 
-go install golang.org/x/tools/cmd/godoc@latest
+if [[ "$mode" == "default" ]]; then
+    brew install golangci-lint gotestsum mockery staticcheck
+    go install golang.org/x/tools/cmd/godoc@latest
+elif [[ "$mode" == "ci" ]]; then
+    go install gotest.tools/gotestsum@latest
+fi
