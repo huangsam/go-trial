40d849efe6fe33c15ddbed3b16a9e53c9c32820c
Samuel Huang <samhuang91@gmail.com>
2025-01-26 19:34:33 -0800

    Optimize setup experience for devs

--- setup.sh
+++ setup.sh
@@ -3,9 +3,26 @@ set -eu
 
 mode="${1:-default}"
 
-if [[ "$mode" == "default" ]]; then
-    brew install golangci-lint gotestsum mockery staticcheck
-    go install golang.org/x/tools/cmd/godoc@latest
-elif [[ "$mode" == "ci" ]]; then
-    go install gotest.tools/gotestsum@latest
-fi
+check_and_install_brew() {
+    [[ -n "$(which "$1")" ]] && return
+    brew install "$1"
+}
+
+check_and_install_go() {
+    [[ -n "$(which "$1")" ]] && return
+    go install "$2"
+}
+
+case "$mode" in
+    "default")
+        brew_list=("golangci-lint" "gotestsum" "mockery" "staticcheck")
+        for brew in "${brew_list[@]}"; do
+            check_and_install_brew "$brew"
+        done
+        check_and_install_go "godoc" "golang.org/x/tools/cmd/godoc@latest"
+        ;;
+    "ci")
+        check_and_install_go "gotestsum" "gotest.tools/gotestsum@latest" ;;
+    *)
+        echo "Invalid mode '$mode' detected" && exit 1 ;;
+esac
