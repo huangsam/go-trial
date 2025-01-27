bac5d231a471fd2391ceef3fbc5e99ce052df612
Samuel Huang <samhuang91@gmail.com>
2025-01-26 22:05:07 -0800

    Revert "Clarify package name for koanf import"
    This reverts commit 2301461082e45b2971a3a888ea02f5b6efdd53bb.

--- pkg/realworld/config_test.go
+++ pkg/realworld/config_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/knadh/koanf/parsers/json"
 	"github.com/knadh/koanf/parsers/yaml"
 	"github.com/knadh/koanf/providers/file"
-	koanf "github.com/knadh/koanf/v2"
+	"github.com/knadh/koanf/v2"
 	"github.com/stretchr/testify/assert"
 )
 
