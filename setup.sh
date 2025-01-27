37e14016c4b21e52b6e8f559bb7d720c1724d61b
Samuel Huang <samhuang91@gmail.com>
2025-01-26 20:29:20 -0800

    Refactor setup experience again

--- setup.sh
+++ setup.sh
@@ -3,26 +3,33 @@ set -eu
 
 mode="${1:-default}"
 
-check_and_install_brew() {
+declare -a brew_queue
+declare -a go_queue
+
+check_and_add_brew() {
     [[ -n "$(which "$1")" ]] && return
-    brew install "$1"
+    brew_queue+=("$1")
 }
 
-check_and_install_go() {
+check_and_add_go() {
     [[ -n "$(which "$1")" ]] && return
-    go install "$2"
+    go_queue+=("$2")
 }
 
 case "$mode" in
     "default")
         brew_list=("golangci-lint" "gotestsum" "mockery" "staticcheck")
         for brew in "${brew_list[@]}"; do
-            check_and_install_brew "$brew"
+            check_and_add_brew "$brew"
         done
-        check_and_install_go "godoc" "golang.org/x/tools/cmd/godoc@latest"
+        check_and_add_go "godoc" "golang.org/x/tools/cmd/godoc@latest"
         ;;
     "ci")
-        check_and_install_go "gotestsum" "gotest.tools/gotestsum@latest" ;;
+        check_and_add_go "gotestsum" "gotest.tools/gotestsum@latest" ;;
     *)
         echo "Invalid mode '$mode' detected" && exit 1 ;;
 esac
+
+# https://stackoverflow.com/a/15780028/2748860
+[[ -n "${brew_queue[*]:-}" ]] && brew install "${brew_queue[@]}"
+[[ -n "${go_queue[*]:-}" ]] && go install "${go_queue[@]}"
