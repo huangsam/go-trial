89306653b610dec9197ce20ae5a3191b6af150f9
Samuel Huang <samhuang91@gmail.com>
2025-02-03 01:06:21 -0800

    Reformat the middleware slightly

--- internal/util/http_serve.go
+++ internal/util/http_serve.go
@@ -7,15 +7,16 @@ import (
 )
 
 // ZerologMiddleware emits a log for each incoming HTTP request.
-var ZerologMiddleware echo.MiddlewareFunc = middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-	LogURI:    true,
-	LogStatus: true,
-	LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-		log.Info().
-			Str("uri", v.URI).
-			Int("status", v.Status).
-			Msg("Got request")
+var ZerologMiddleware echo.MiddlewareFunc = middleware.RequestLoggerWithConfig(
+	middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			log.Info().
+				Str("uri", v.URI).
+				Int("status", v.Status).
+				Msg("Got request")
 
-		return nil
-	},
-})
+			return nil
+		},
+	})
