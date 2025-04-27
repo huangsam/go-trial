91f008dc0c0abcf190c02248dc241db4fdcef3f3
Samuel Huang <samhuang91@gmail.com>
2025-04-26 23:45:49 -0700

    Migrate to mockery v3 logic

--- pkg/abstraction/mocking_repository_test.go
+++ pkg/abstraction/mocking_repository_test.go
@@ -3,14 +3,13 @@ package abstraction_test
 import (
 	"testing"
 
-	abstraction_mock "github.com/huangsam/go-trial/mock/abstraction"
 	"github.com/huangsam/go-trial/pkg/abstraction"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetUserByID(t *testing.T) {
-	mockRepo := new(abstraction_mock.MockUserRepository)
+	mockRepo := new(abstraction.MockUserRepository)
 	user := &abstraction.User{ID: 1, Name: "John Doe", Email: "john.doe@example.com"}
 
 	mockRepo.On("GetUserByID", 1).Return(user, nil)
@@ -23,7 +22,7 @@ func TestGetUserByID(t *testing.T) {
 }
 
 func TestCreateUser(t *testing.T) {
-	mockRepo := new(abstraction_mock.MockUserRepository)
+	mockRepo := new(abstraction.MockUserRepository)
 	user := &abstraction.User{ID: 2, Name: "Jane Doe", Email: "jane.doe@example.com"}
 
 	mockRepo.On("CreateUser", user).Return(nil)
