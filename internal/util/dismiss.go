accf6a466726542073845a7a8c979fec8999ce09
Samuel Huang <samhuang91@gmail.com>
2025-04-30 09:15:36 -0700

    Add test for Dismiss

--- internal/util/dismiss.go
+++ internal/util/dismiss.go
@@ -1,5 +1,9 @@
 package util
 
+// Dismiss calls a function and ignores its error.
+//
+// This is useful for functions that close a resource and return an error that
+// we don't care about, such as closing a file or a network connection.
 func Dismiss(f func() error) {
 	_ = f()
 }
---
+++ internal/util/dismiss_test.go
@@ -0,0 +1,28 @@
+package util_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/huangsam/go-trial/internal/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDismiss(t *testing.T) {
+	var called bool
+
+	// Test case where the function succeeds
+	util.Dismiss(func() error {
+		called = true
+		return nil
+	})
+	assert.True(t, called, "The function should have been called")
+
+	// Test case where the function returns an error
+	called = false
+	util.Dismiss(func() error {
+		called = true
+		return errors.New("some error")
+	})
+	assert.True(t, called, "The function should have been called even if it returns an error")
+}
