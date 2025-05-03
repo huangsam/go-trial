57defd579fcadd713737d30779b34631e4611a61
Samuel Huang <samhuang91@gmail.com>
2025-05-03 02:36:55 -0700

    Add useful docs for shape payload

--- internal/model/shape.go
+++ internal/model/shape.go
@@ -3,6 +3,9 @@ package model
 import "github.com/huangsam/go-trial/pkg/abstraction"
 
 // ShapePayload is the JSON response for any handler that deals with shapes.
+//
+// The dimensions field is an interface to allow for different shapes.
+// The size field indicates the shape classification (e.g., small, medium, large).
 type ShapePayload struct {
 	Area       float64           `json:"area"`
 	Perimeter  float64           `json:"perimeter"`
@@ -11,6 +14,9 @@ type ShapePayload struct {
 }
 
 // NewShapePayload creates a new ShapePayload from a Shape.
+//
+// The endpoint showcases the power of interfaces in Go.
+// It allows us to handle different shapes (e.g., Rectangle, Circle) uniformly.
 func NewShapePayload(shape abstraction.Shape) ShapePayload {
 	return ShapePayload{
 		Area:       shape.Area(),
