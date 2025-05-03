55117d9c6d1e719a4afcba86591854bbff2ae989
Samuel Huang <samhuang91@gmail.com>
2025-05-03 03:00:23 -0700

    Add docs to UserAccount struct

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,6 +1,12 @@
 package model
 
 // UserAccount is an account with username and password.
+//
+// It is used to represent a user account in the system.
+// The username is a unique identifier for the user, and the password is used for authentication.
+//
+// This struct is used for demonstration purposes only.
+// In production, the password should be hashed and salted for security.
 type UserAccount struct {
 	Username string
 	Password string
