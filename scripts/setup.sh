44ca4752a8090b7eefb1cb5ccd867b62e2cecea8
Samuel Huang <samhuang91@gmail.com>
2025-05-03 16:56:18 -0700

    Try out again but with sudo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,7 +11,8 @@ case "$mode" in
         go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
         ;;
     ci)
-        apt-get update && apt-get install -y protobuf-compiler protoc-gen-go
+        sudo apt-get update
+        sudo apt-get install -y protobuf-compiler protoc-gen-go
         go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
         go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
         ;;
