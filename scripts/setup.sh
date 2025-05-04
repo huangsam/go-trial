5d4d1b0a045a5d0abfac18573ae406f339b6b678
Samuel Huang <samhuang91@gmail.com>
2025-05-03 18:53:57 -0700

    -sSL option to curl

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,8 +11,7 @@ case "$mode" in
         go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
         ;;
     ci)
-        sudo apt-get update
-        curl https://github.com/protocolbuffers/protobuf/releases/download/v30.2/protoc-30.2-linux-x86_64.zip -o protoc.zip
+        curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v30.2/protoc-30.2-linux-x86_64.zip -o protoc.zip
         unzip protoc.zip -d "$HOME/.protoc"
         chmod +x "$HOME/.protoc/bin/protoc"
         go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
