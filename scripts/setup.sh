053addec55ee56ca2044477fa753b6f00aad79d1
Samuel Huang <samhuang91@gmail.com>
2025-05-03 18:51:50 -0700

    Optimize protoc setup in CI

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,7 +12,9 @@ case "$mode" in
         ;;
     ci)
         sudo apt-get update
-        sudo apt-get install -y protobuf-compiler protoc-gen-go
+        curl https://github.com/protocolbuffers/protobuf/releases/download/v30.2/protoc-30.2-linux-x86_64.zip -o protoc.zip
+        unzip protoc.zip -d "$HOME/.protoc"
+        chmod +x "$HOME/.protoc/bin/protoc"
         go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
         go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
         ;;
