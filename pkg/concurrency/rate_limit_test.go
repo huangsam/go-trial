5bc5bffc4c474e8138096e816e3eb593e7456af7
Samuel Huang <samhuang91@gmail.com>
2025-05-04 23:24:52 -0700

    Adjust test run names

--- pkg/concurrency/rate_limit_test.go
+++ pkg/concurrency/rate_limit_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestRateLimitCounter(t *testing.T) {
-	t.Run("RateLimitCounter with limit 5 and rate 100ms", func(t *testing.T) {
+	t.Run("Limit5Rate100ms", func(t *testing.T) {
 		limit := 5
 		rate := 100 * time.Millisecond
 		result := concurrency.RateLimitCounter(limit, rate)
@@ -18,6 +18,20 @@ func TestRateLimitCounter(t *testing.T) {
 		// the number of increments possible within that time frame.
 		expectedMin := 10 // 1 second / 100ms = 10 increments
 		expectedMax := 15 // Allowing for some buffer due to timing variations
+
+		assert.GreaterOrEqual(t, result, expectedMin)
+		assert.LessOrEqual(t, result, expectedMax)
+	})
+
+	t.Run("Limit5Rate200ms", func(t *testing.T) {
+		limit := 5
+		rate := 200 * time.Millisecond
+		result := concurrency.RateLimitCounter(limit, rate)
+
+		// Since the function runs for 1 second, we expect the result to be approximately
+		// the number of increments possible within that time frame.
+		expectedMin := 5  // 1 second / 200ms = 5 increments
+		expectedMax := 10 // Allowing for some buffer due to timing variations
 
 		assert.GreaterOrEqual(t, result, expectedMin)
 		assert.LessOrEqual(t, result, expectedMax)
