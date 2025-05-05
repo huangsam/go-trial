15267ebdc36dd5ed5692f23b668d5c585a3a8b30
Samuel Huang <samhuang91@gmail.com>
2025-05-04 17:36:14 -0700

    Move interceptors into grpc_server

--- pkg/endpoint/grpc_server.go
+++ pkg/endpoint/grpc_server.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	pb "github.com/huangsam/go-trial/pkg/endpoint/proto"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 )
 
@@ -39,4 +40,20 @@ func (s *EchoerServer) EchoStream(stream grpc.BidiStreamingServer[pb.EchoRequest
 			return err
 		}
 	}
+}
+
+// LogUnaryRequest is a gRPC unary server interceptor that logs the request method.
+var LogUnaryRequest grpc.UnaryServerInterceptor = func(
+	ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler,
+) (resp any, err error) {
+	log.Info().Msgf("gRPC request: %s", info.FullMethod)
+	return handler(ctx, req)
+}
+
+// LogStreamRequest is a gRPC stream server interceptor that logs the request method.
+var LogStreamRequest grpc.StreamServerInterceptor = func(
+	srv any, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler,
+) error {
+	log.Info().Msgf("gRPC stream request: %s", info.FullMethod)
+	return handler(srv, ss)
 }
