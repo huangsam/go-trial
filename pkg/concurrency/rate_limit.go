52f75959098ecfb6b6f791d86f3f18c6b2ce7343
Samuel Huang <samhuang91@gmail.com>
2025-05-06 08:04:31 -0700

    Refactor comments in RateLimitCounter

--- pkg/concurrency/rate_limit.go
+++ pkg/concurrency/rate_limit.go
@@ -11,16 +11,21 @@ import (
 // The counter will increment as long as the channel has space.
 // The function will return the count of increments after 1 second.
 func RateLimitCounter(limit int, rate time.Duration) int {
+	// Fill the channel to its limit for bursty behavior
 	limitChan := make(chan struct{}, limit)
 	for range limit {
 		limitChan <- struct{}{}
-	} // Fill the channel to its limit for bursty behavior
+	}
+
+	// Replenish the channel at the specified rate
 	go func() {
 		ticker := time.NewTicker(rate)
 		for range ticker.C {
 			limitChan <- struct{}{}
-		} // Replenish the channel at the specified rate
+		}
 	}()
+
+	// Count the number of increments until the context times out
 	result := 0
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
