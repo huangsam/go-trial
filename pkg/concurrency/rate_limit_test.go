181507318e19ce59f5b20f00dde0cfe9ad34ffb3
Samuel Huang <samhuang91@gmail.com>
2025-05-06 08:01:11 -0700

    Refactor RateLimitCounter tests

--- pkg/concurrency/rate_limit_test.go
+++ pkg/concurrency/rate_limit_test.go
@@ -9,30 +9,37 @@ import (
 )
 
 func TestRateLimitCounter(t *testing.T) {
+	limit := 5 // Burst of up to 5 increments
+
+	// Since the function runs for 1 second, we expect the result to be approximately
+	// the number of increments possible within that time frame.
+
+	t.Run("Limit5Rate50ms", func(t *testing.T) {
+		rate := 50 * time.Millisecond
+		result := concurrency.RateLimitCounter(limit, rate)
+
+		expectedMin := 20 // 1 second / 50ms = 20 increments
+		expectedMax := 25 // Allow for some buffer due to timing variations
+		assert.GreaterOrEqual(t, result, expectedMin)
+		assert.LessOrEqual(t, result, expectedMax)
+	})
+
 	t.Run("Limit5Rate100ms", func(t *testing.T) {
-		limit := 5
 		rate := 100 * time.Millisecond
 		result := concurrency.RateLimitCounter(limit, rate)
 
-		// Since the function runs for 1 second, we expect the result to be approximately
-		// the number of increments possible within that time frame.
 		expectedMin := 10 // 1 second / 100ms = 10 increments
-		expectedMax := 15 // Allowing for some buffer due to timing variations
-
+		expectedMax := 15 // Allow for some buffer due to timing variations
 		assert.GreaterOrEqual(t, result, expectedMin)
 		assert.LessOrEqual(t, result, expectedMax)
 	})
 
 	t.Run("Limit5Rate200ms", func(t *testing.T) {
-		limit := 5
 		rate := 200 * time.Millisecond
 		result := concurrency.RateLimitCounter(limit, rate)
 
-		// Since the function runs for 1 second, we expect the result to be approximately
-		// the number of increments possible within that time frame.
 		expectedMin := 5  // 1 second / 200ms = 5 increments
-		expectedMax := 10 // Allowing for some buffer due to timing variations
-
+		expectedMax := 10 // Allow for some buffer due to timing variations
 		assert.GreaterOrEqual(t, result, expectedMin)
 		assert.LessOrEqual(t, result, expectedMax)
 	})
